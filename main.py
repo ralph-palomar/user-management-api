488733bfb154d72ec6b4363024f20616433ab5ce
Ralph Palomar <rpalomar@pepper.com.au>
2020-05-23 18:15:20 +0800

    Add JWT authorization

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from cryptography.fernet import Fernet
+from jwt import InvalidSignatureError, DecodeError
 from waitress import serve
 import pymongo
 import os
 import base64
+import jwt
 from functools import wraps
 
 app = Flask(__name__)
@@ -12,6 +14,22 @@ m_client = pymongo.MongoClient('mongodb://localhost:27017/')
 m_db = m_client[os.environ['DB_NAME']]
 key = base64.b64encode(bytes(os.environ['SECRET_KEY'], 'utf8'))
 cipher_suite = Fernet(key)
+
+
+def requires_jwt(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            auth = request.headers['JWT']
+            jwt.decode(auth, os.environ['SECRET_KEY'], algorithm='HS256')
+        except KeyError:
+            return unauthorized()
+        except InvalidSignatureError:
+            return unauthorized()
+        except DecodeError:
+            return unauthorized()
+        return f(*args, **kwargs)
+    return wrapper
 
 
 def requires_client_credentials(f):
@@ -38,6 +56,7 @@ def preflight():
 
 @app.route('/user-management/api/v1/users/all', methods=['GET'])
 @requires_client_credentials
+@requires_jwt
 def get_all_users():
     limit = int(request.args.get('limit')) if request.args.get('limit') is not None else 10
     offset = int(request.args.get('offset')) if request.args.get('offset') is not None else 0
@@ -63,6 +82,12 @@ def get_users():
         else:
             response_payload = result
             del response_payload['password']
+            jwt_payload = {
+                "email": response_payload['email'],
+                "password": result_password
+            }
+            access_token = jwt.encode(jwt_payload, os.environ['SECRET_KEY'], algorithm='HS256')
+            response_payload['access_token'] = str(access_token, 'UTF-8')
     else:
         response_payload = {
             "error": "Account does not exist"
@@ -90,8 +115,18 @@ def create_user():
     return create_response(response_payload), 200
 
 
+@app.route('/user-management/api/v1/users', methods=['PUT'])
+@requires_client_credentials
+@requires_jwt
+def update_user():
+    update_user_data(request.json)
+    response_payload = {}
+    return create_response(response_payload), 200
+
+
 @app.route('/user-management/api/v1/basicInfo', methods=['POST'])
 @requires_client_credentials
+@requires_jwt
 def create_basic_info():
     update_or_insert_data(request.json, 'basic_info')
     response_payload = {}
@@ -100,6 +135,7 @@ def create_basic_info():
 
 @app.route('/user-management/api/v1/illnesses', methods=['POST'])
 @requires_client_credentials
+@requires_jwt
 def create_illnesses():
     update_or_insert_data(request.json, 'illnesses')
     response_payload = {}
@@ -108,6 +144,7 @@ def create_illnesses():
 
 @app.route('/user-management/api/v1/medications', methods=['POST'])
 @requires_client_credentials
+@requires_jwt
 def create_medications():
     update_or_insert_data(request.json, 'medications')
     response_payload = {}
@@ -116,6 +153,7 @@ def create_medications():
 
 @app.route('/user-management/api/v1/vitalSigns', methods=['POST'])
 @requires_client_credentials
+@requires_jwt
 def create_vital_signs():
     update_or_insert_data(request.json, 'vital_signs')
     response_payload = {}
@@ -124,6 +162,7 @@ def create_vital_signs():
 
 @app.route('/user-management/api/v1/diet', methods=['POST'])
 @requires_client_credentials
+@requires_jwt
 def create_diet():
     update_or_insert_data(request.json, 'diet')
     response_payload = {}
@@ -132,6 +171,7 @@ def create_diet():
 
 @app.route('/user-management/api/v1/others', methods=['POST'])
 @requires_client_credentials
+@requires_jwt
 def create_other_questions():
     update_or_insert_data(request.json, 'other_questions')
     response_payload = {}
@@ -147,8 +187,18 @@ def update_or_insert_data(payload, collection):
     }, upsert=True)
 
 
+def update_user_data(payload):
+    m_query = {
+        "email": payload['email']
+    }
+    m_db['users'].update_one(m_query, {
+       "$set": payload
+    })
+
+
 @app.route('/user-management/api/v1/basicInfo', methods=['GET'])
 @requires_client_credentials
+@requires_jwt
 def get_basic_info():
     response_payload = query_data('basic_info')
     return create_response(response_payload), 200
@@ -156,6 +206,7 @@ def get_basic_info():
 
 @app.route('/user-management/api/v1/illnesses', methods=['GET'])
 @requires_client_credentials
+@requires_jwt
 def get_illnesses():
     response_payload = query_data('illnesses')
     return create_response(response_payload), 200
@@ -163,6 +214,7 @@ def get_illnesses():
 
 @app.route('/user-management/api/v1/medications', methods=['GET'])
 @requires_client_credentials
+@requires_jwt
 def get_medications():
     response_payload = query_data('medications')
     return create_response(response_payload), 200
@@ -170,6 +222,7 @@ def get_medications():
 
 @app.route('/user-management/api/v1/vitalSigns', methods=['GET'])
 @requires_client_credentials
+@requires_jwt
 def get_vital_signs():
     response_payload = query_data('vital_signs')
     return create_response(response_payload), 200
@@ -177,6 +230,7 @@ def get_vital_signs():
 
 @app.route('/user-management/api/v1/diet', methods=['GET'])
 @requires_client_credentials
+@requires_jwt
 def get_diet():
     response_payload = query_data('diet')
     return create_response(response_payload), 200
@@ -184,6 +238,7 @@ def get_diet():
 
 @app.route('/user-management/api/v1/others', methods=['GET'])
 @requires_client_credentials
+@requires_jwt
 def get_other_questions():
     response_payload = query_data('other_questions')
     return create_response(response_payload), 200
@@ -214,7 +269,7 @@ def create_response(response_payload, deleted_keys=['_id']):
 
     response = jsonify(response_payload)
     response.headers['Access-Control-Allow-Origin'] = os.environ['ALLOWED_ORIGIN']
-    response.headers['Access-Control-Allow-Headers'] = 'email, password, Authorization, Overwrite, Destination, Content-Type, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If'
+    response.headers['Access-Control-Allow-Headers'] = 'email, password, Authorization, JWT, Overwrite, Destination, Content-Type, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If'
     response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS, POST, PUT'
     response.headers['Access-Control-Max-Age'] = 3600
     return response
